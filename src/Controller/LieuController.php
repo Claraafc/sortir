8ba9543b767f80a8d7bcc78de2f689e300d3dbc8
cacosta <claraaf@gmail.com>
2019-10-23 09:48:08 +0200

    sortie et lieu form, controller and twig

---
+++ src/Controller/LieuController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Lieu;
+use App\Form\LieuType;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LieuController extends Controller
+{
+    /**
+     * @Route("/lieu", name="lieu")
+     */
+    public function index(Request $request)
+    {
+        $lieu = new Lieu();
+        $lieuForm = $this->createForm(LieuType::class, $lieu);
+        $lieuForm->handleRequest($request);
+
+        if($lieuForm->isSubmitted() && $lieuForm->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($lieu);
+            $em->flush();
+            return $this->redirectToRoute();
+        }
+        return $this->render('lieu/index.html.twig', [
+            'lieuForm' => $lieuForm->createView()
+        ]);
+    }
+}
